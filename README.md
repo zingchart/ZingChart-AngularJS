10f3c089ee8c133d1b41e7ec52a71bec4efd519d
AppleCider <michaelschultz01@gmail.com>
2015-02-27 16:18:16 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,118 @@
+#ZingChart-AngularJS
+---
+An AngularJS directive for ZingChart
+### Install
+```
+bower install zingchart-angularjs
+```
+
+Inject into your app...
+```
+var app = angular.module('myApp', ['zingchart-angularjs']);
+```
+
+###Usage
+---
+_javscript_
+```js
+//In an Angular Controller
+$scope.myValues = [5,6,3,2,3];
+```
+
+_markup_
+```html
+<zingchart id="chart-1 zc-values="myValues"></zingchart>
+```
+
+###Options
+---
+The ZingChart Component takes the following attributes:
+
+#####_zc-id_ [string] ```(required)```
+The id for the DOM element for ZingChart to attach to.
+
+#####_zc-values_ [array] ```(optional)```
+```default : null```
+
+Either a single-dimensional or multi-dimensional array containing the values to be charted. **Must be an Angular scope variable to bind to the directive** Overrides the series values in the zc-render and zc-data objects.
+```
+$scope.myData = [0,2,2,3,3,4];
+$scope.myData2 = [[45,43,26],[0,1,5,3]];
+```
+This parameter simulates the values parameter in each series object in a ZingChart json.
+```js
+//ZingChart json example
+data:{
+    series : [
+        {'values' : [45,43,26]},
+        {'values' : [0,1,5,3]}
+    ]
+}
+```
+The directive takes care of the work so you don't have to create this object
+
+#####_zc-json [object] ```(optional)```
+```default : null```
+
+A ZingChart json object. This is the same object you would use to configure a chart using zingchart.render.data. It is a pseudo-parent object of zc-values. More information : http://www.zingchart.com/docs/json-attributes-syntax/
+```js
+//Example
+$scope.json = {
+  "series":[
+        {
+            "line-color":"#900000",
+            "marker":{
+                "background-color":"#dc3737",
+                "border-width":1,
+                "shadow":0,
+                "border-color":"#f56b6b"
+            }
+        },
+        {
+            "line-color":"#efe634",
+            "marker":{
+                "background-color":"#fff41f",
+                "border-width":1,
+                "shadow":0,
+                "border-color":"#fdffc0"
+            }
+        },
+    ]
+}
+```
+Note: You can add series values into this object like you normally would while using ZingChart. However if you define the directives zc-values parameter, those values will override and "values" inside of your zc-data object
+
+
+#####_zc-render_ [object] ```(optional)```
+```default : null```
+
+A ZingChart render object. This is the same object you would use to configure a chart using zingchart.render. You can change the render type, add events, and change other zingchart properties in here. Acts like a pseudo-parent of zc-values and zc-data. zc-render's properties will be overwritten if zc-values and zc-data are defined. More information : http://www.zingchart.com/docs/reference/zingchart-object/#zingchart__render
+
+Note: This object will not be watched inside the directive. It is a one-time setup.
+
+```
+//Example
+{
+    output :'canvas',
+    events: {
+        complete : function(p) {...}
+    }
+}
+```
+
+
+
+#####_zc-height_ [number] ```(optional)```
+```default : 400```
+
+Will override the height inside of a zc-render object if defined.
+
+#####_zc-width_ [number] ```(optional)```
+```default : 600```
+
+Will override the width inside of a zc-render object if defined.
+
+#####_zc-type_ [string] ```(optional)```
+```default : line```
+
+Will override the render type inside of a zc-render and zc-data object if defined.
---
+++ test/defaults.js
@@ -0,0 +1,274 @@
+describe('ZingChart Directive', function(){
+
+    beforeEach(module('zingchart-angularjs'));
+
+    var _$scope;
+    var _$compile;
+
+    beforeEach(function(done){
+        inject(function ($compile, $rootScope) {
+            _$scope = $rootScope.$new();
+            _$compile = $compile;
+            var completed = 0;
+            $('<div id="container"></div>').appendTo('body');
+
+            //Cases 1+2
+
+            $('<zingchart id="chart-1"></zingchart>').appendTo("#container");
+
+            //Case 3
+            _$scope.myValues2 = [[3,2,3,3,9] , [1,2,3,4,5]];
+            $('<zingchart id="chart-2" zc-values="myValues2"></zingchart>').appendTo("#container");
+
+            //Case 4
+            _$scope.myValues3 = [1,2,3,4,5];
+            $('<zingchart id="chart-3" zc-values="myValues3"></zingchart>').appendTo("#container");
+
+            //Case 5
+            _$scope.myValues4 = [1,2,3,4,5];
+            _$scope.myJson4 = {
+                    "series":[
+                        {
+                            "line-color":"#ff653f",
+                            "marker":{
+                                "background-color":"#900000",
+                                "border-width":1,
+                                "shadow":0,
+                                "border-color":"#f56b6b"
+                            }
+                        }
+                    ]
+            };
+            $('<zingchart id="chart-4" zc-values="myValues4" zc-json="myJson4"></zingchart>').appendTo("#container");
+
+            //Case 6
+            _$scope.myValues5 = [5,5,6,7,10];
+            _$scope.myJson5 = {
+                    "series":[
+                        {
+                            "line-color":"#007790",
+                            "marker":{
+                                "background-color":"#007790",
+                                "border-width":1,
+                                "shadow":0,
+                                "border-color":"#69dbf1"
+                            },
+                            "values": [0,0,0,2,3]
+                        },
+                        {
+                            "line-color":"#007790",
+                            "marker":{
+                                "background-color":"#007790",
+                                "border-width":1,
+                                "shadow":0,
+                                "border-color":"#69dbf1"
+                            },
+                            "values" : [1,2,3,4,5]
+                        }
+                    ]
+            };
+            $('<zingchart id="chart-5" zc-values="myValues5" zc-json="myJson5"></zingchart>').appendTo("#container");
+
+            //Case 7
+            _$scope.myValues6 = [[10,20,30,44,99],[20,30,50,20,50]];
+            _$scope.myJson6 = {
+                    "series":[
+                        {
+                            "line-color":"#33d911",
+                            "marker":{
+                                "background-color":"#90e496",
+                                "border-width":1,
+                                "shadow":0,
+                                "border-color":"#6bf56e"
+                            },
+                            "values": [0,0,0,2,3]
+                        },
+                        {
+                            "line-color":"#90005b",
+                            "marker":{
+                                "background-color":"#8b0090",
+                                "border-width":1,
+                                "shadow":0,
+                                "border-color":"#d6aee7"
+                            },
+                            "values" : [1,2,3,4,5]
+                        }
+                    ]
+            };
+            _$scope.myRender6 = {
+                "output" : "canvas",
+                "data" : {
+                    "series" : [{
+                        "values" : [1,1,1]
+                    }]
+                }
+            };
+            $('<zingchart id="chart-6" zc-values="myValues6" zc-json="myJson6" zc-render="myRender6"></zingchart>').appendTo("#container");
+
+            //Case 8
+            _$scope.myValues7 = [[3,2,3,3,9] , [1,2,3,4,5]];
+            $('<zingchart id="chart-7" zc-values="myValues7" zc-type="bar"></zingchart>').appendTo("#container");
+
+
+
+            var $element = _$compile(document.getElementById('container'))(_$scope);
+
+            zingchart.complete=function(p){
+                completed++;
+                if(completed == 7){
+                    done();
+                }
+            }
+        });
+    });
+    //Case 1
+    it("should create chart-1 : render an empty zingchart", function(){
+        var output = zingchart.exec('chart-1', 'getdata');
+        var expected = {
+            "graphset": [{
+                "type":"line",
+                "series":[]
+            }]
+        };
+        expect(JSON.stringify(output)).to.equal(JSON.stringify(expected));
+
+    });
+
+    //Case 2
+    it("should render chart-1 with default width and height", function(){
+        var output = zingchart.exec('chart-1', 'getobjectinfo', {
+            object : 'graph'
+        });
+        expect(output.width).to.equal(600);
+        expect(output.height).to.equal(400);
+    });
+
+    //Case 3
+    it("should render chart-2, a line chart with 2 series", function(){
+        var output = zingchart.exec('chart-2', 'getdata');
+        var expected = {
+            "graphset": [{
+                "type":"line",
+                "series":[
+                    {"values" : [3,2,3,3,9]},
+                    {"values" : [1,2,3,4,5]},
+                ]
+            }]
+        };
+        expect(JSON.stringify(output)).to.equal(JSON.stringify(expected));
+    });
+
+    //Case 4
+    it("should render chart-3; a line chart with 1 series", function(){
+        var output = zingchart.exec('chart-3', 'getdata');
+        var expected = {
+            "graphset": [{
+                "type":"line",
+                "series":[
+                    {"values" : [1,2,3,4,5]},
+                ]
+            }]
+        };
+        expect(JSON.stringify(output)).to.equal(JSON.stringify(expected));
+    });
+
+    //Case 5
+    it("should render chart-4; a line chart with 1 series, and a data object", function(){
+        var output = zingchart.exec('chart-4', 'getdata');
+        var expected = {
+            "graphset":[{
+                "type":"line",
+                "series":[{
+                    "line-color":"#ff653f",
+                    "marker":{
+                        "background-color":"#900000",
+                        "border-width":1,
+                        "shadow":0,
+                        "border-color":"#f56b6b"
+                    },
+                "values":[1,2,3,4,5]}
+                ]}
+            ]
+        };
+        expect(JSON.stringify(output)).to.equal(JSON.stringify(expected));
+    });
+
+    //Case 6
+    it("should render chart-5; a line chart with 1 series, and a data object with 2 series", function(){
+        var output = zingchart.exec('chart-5', 'getdata');
+        var expected = {
+            "graphset":[{
+                "type":"line",
+                "series":[
+                    {
+                        "line-color":"#007790",
+                        "marker":{
+                            "background-color":"#007790",
+                            "border-width":1,
+                            "shadow":0,
+                            "border-color":"#69dbf1"
+                        },
+                        "values":[5,5,6,7,10]
+                    },
+                    {
+                        "line-color":"#007790",
+                        "marker":{
+                            "background-color":"#007790",
+                            "border-width":1,
+                            "shadow":0,
+                            "border-color":"#69dbf1"
+                        },
+                        "values":[1,2,3,4,5]
+                    }
+                ]}
+            ]
+        };
+        expect(JSON.stringify(output)).to.equal(JSON.stringify(expected));
+    });
+
+    //Case 7
+    it("should render chart-6; a line chart with 2 series, and a data object with 2 series, and a render object with 1 series in canvas", function(){
+        var output = zingchart.exec('chart-6', 'getdata');
+        var expected = {
+            "graphset": [{
+                "type": "line",
+                "series": [{
+                    "line-color": "#33d911",
+                    "marker": {
+                        "background-color": "#90e496",
+                        "border-width": 1,
+                        "shadow": 0,
+                        "border-color": "#6bf56e"
+                    },
+                    "values": [10, 20, 30, 44, 99]
+                }, {
+                    "line-color": "#90005b",
+                    "marker": {
+                        "background-color": "#8b0090",
+                        "border-width": 1,
+                        "shadow": 0,
+                        "border-color": "#d6aee7"
+                    },
+                    "values": [20, 30, 50, 20, 50]
+                }]
+            }]
+        };
+        expect(JSON.stringify(output)).to.equal(JSON.stringify(expected));
+    });
+
+    //Case 7
+    it("should render chart-7 a 1 series bar chart", function(){
+        var output = zingchart.exec('chart-7', 'getdata');
+        var expected = {
+            "graphset": [{
+                "type": "bar",
+                "series": [{
+                    "values": [3, 2, 3, 3, 9]
+                }, {
+                    "values": [1, 2, 3, 4, 5]
+                }]
+            }]
+        };
+        expect(JSON.stringify(output)).to.equal(JSON.stringify(expected));
+    });
+});
